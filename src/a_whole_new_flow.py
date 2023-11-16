dd522c26b3ed2fbed43b114394d4584124ba1842
Chris Boyd <chris.b@prefect.io>
2023-11-16 16:17:28 -0500

    update

--- src/a_whole_new_flow.py
+++ src/a_whole_new_flow.py
@@ -4,6 +4,7 @@ from prefect import flow, get_run_logger
 from os import environ
 from prefect.runner.storage import GitRepository
 
+
 @flow
 def get_random_pun():
     logger = get_run_logger()
@@ -19,12 +20,10 @@ def get_random_pun():
         
         joke_data = response.json()
         logger.info(f"{joke_data.get('joke', 'No joke found')}")
-        return joke_data.get("joke", "No joke found")
     except requests.exceptions.RequestException as e:
         logger.error(f"Error: {e}")
         return f"Error: {e}"
 
-# In GHA
 if __name__ == "__main__":
     TIER = environ.get('WORK_POOL', 'dev')
     flow.from_source(
@@ -38,21 +37,3 @@ if __name__ == "__main__":
         work_pool_name=f"{TIER}",
         build=False
     )
-
-
-# # LOCAL Deploy
-# if __name__ == "__main__":
-#     TIER = 'dev'
-#     flow.from_source(
-#         source=GitRepository(
-#             url="https://github.com/prefectcboyd/prefectcicd.git",
-#             # branch=environ.get("BRANCH_REF", "feature-123").split('/')[-1]
-#             branch="feature-123"
-#         ),
-#         entrypoint="src/a_whole_new_flow:get_random_pun",
-#     ).deploy(
-#         name=f"a_whole_new_flow_{TIER}",
-#         work_pool_name=f"{TIER}",
-#         build=False
-#     )
-# #
